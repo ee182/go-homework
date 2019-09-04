19922d819b494fc0477aa47ad640c248bfba0d04
quantum-craft <y_ee182@hotmail.com>
2019-09-04 16:05:22 +0800

    Finish ValueEqIndex

--- week2opt/week2opt.go
+++ week2opt/week2opt.go
@@ -1,6 +1,24 @@
 package week2opt
 
 // ValueEqIndex checks whether or not there is an index i such that A[i] = i
-func ValueEqIndex(a []int) bool {
-	return false
+func ValueEqIndex(a []int, idx []int) (bool, int) {
+	if len(a) == 0 {
+		return false, -1
+	}
+
+	if len(a) == 1 {
+		return a[0] == idx[0], idx[0]
+	}
+
+	var middleI = len(a) / 2
+
+	if a[middleI] > idx[middleI] {
+		return ValueEqIndex(a[0:middleI], idx[0:middleI])
+	}
+
+	if a[middleI] < idx[middleI] {
+		return ValueEqIndex(a[middleI+1:len(a)], idx[middleI+1:len(a)])
+	}
+
+	return true, idx[middleI]
 }
