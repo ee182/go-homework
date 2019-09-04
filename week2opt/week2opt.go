fcb1f189d3ee27539d9d7a5021e167f908ebc44b
quantum-craft <y_ee182@hotmail.com>
2019-09-04 19:07:47 +0800

    Add Find2ndLargest basic

--- week2opt/week2opt.go
+++ week2opt/week2opt.go
@@ -1,6 +1,14 @@
 package week2opt
 
+// Find2ndLargest finds the second-largest number in the array,
+// and that uses at most n + lg n - 2 comparisons.
+// 1.
+func Find2ndLargest(a []int) int {
+	return a[0]
+}
+
 // ValueEqIndex checks whether or not there is an index i such that A[i] = i
+// 3.
 func ValueEqIndex(a []int, idx []int) (bool, int) {
 	if len(a) == 0 {
 		return false, -1
