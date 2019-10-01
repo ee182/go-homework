e29bc3587f85751afad8b3076a18a9ba01164e95
quantum-craft <y_ee182@hotmail.com>
2019-10-01 22:00:47 +0800

    Added test codes

--- sortalgorithm/test.go
+++ sortalgorithm/test.go
@@ -10,11 +10,27 @@ import (
 
 // Test2 is used to test sortalgorithm.QuickSort()
 func Test2() {
-	xs := []int{3, -9, -10, 8, 2, 22, 3, 22, -100, -100, 100, 12, 12, 13, 14, 15, 15, 14, 13, 12, -1, -2, -3, -4, -5, -1, -5, -4, -3, -2, 12, 3, 4, 5, 6, 7, 5, 1, 44, 4, 44, 7, 6, -9, 10, -10, -15, 16, 17, -1, -15, -1}
+	f, err := os.Open("QuickSortNumbers.txt")
+	if err != nil {
+		fmt.Println("error opening file= ", err)
+		os.Exit(1)
+	}
 
-	QuickSort(xs)
+	r := bufio.NewReader(f)
+	line, err := r.ReadString('\n')
 
-	fmt.Println(xs)
+	numbers := make([]int, 0, 0)
+
+	for err == nil {
+		i, _ := strconv.Atoi(strings.TrimSuffix(line, "\r\n"))
+
+		numbers = append(numbers, i)
+
+		line, err = r.ReadString('\n')
+	}
+
+	QuickSort(numbers)
+	fmt.Println(numbers)
 }
 
 // Test is used to test sortalgorithm.SortAndCount()
